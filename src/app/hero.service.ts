52385000e7e3b1d4d6109a45398aec580a737785
Evgeniy Mednov <evgeniy.mednov@rt.ru>
2018-12-21 11:58:19 +0300

    4th chapter added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,7 +10,7 @@ import {MessageService} from './message.service';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes')
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
