bd860f3117f61f64307f31cfa1767ca32812bac8
Evgeniy Mednov <evgeniy.mednov@rt.ru>
2019-01-11 16:22:55 +0300

    chapter 6 done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,11 +14,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
